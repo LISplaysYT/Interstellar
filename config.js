c86febb0ac016458fedd5187670a81b9cd6ea306
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2024-10-22 12:11:16 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ const config = {
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    LISPlays: "gtg@1766",
+    passwordisdemo: "demo",
   },
 };
 
